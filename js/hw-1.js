4de1f003ba701c819feed1b61feba2db5a130418
MarkUA <mark.d.nahorniak@gmail.com>
2024-09-24 22:25:47 +0300

    fix

--- js/hw-1.js
+++ js/hw-1.js
@@ -1,25 +1,25 @@
-const age = 12
-console.log(age);
+// const age = 12
+// console.log(age);
 
-const name = "Mark"
-console.log(name)
+// const name = "Mark"
+// console.log(name)
 
-const isStudent = confirm("u are student")
-console.log(isStudent)
+// const isStudent = confirm("u are student")
+// console.log(isStudent)
 
-const myString = "Електрика як хліб, вона потрібна кожному"
-console.log(myString)
-console.log(typeof myString)
+// const myString = "Електрика як хліб, вона потрібна кожному"
+// console.log(myString)
+// console.log(typeof myString)
 
-const myNumber = 222
-console.log(myNumber + 10)
+// const myNumber = 222
+// console.log(myNumber + 10)
 
-const myNull = null
-console.log(myNull)
+// const myNull = null
+// console.log(myNull)
 
-const wathName = prompt("Wath are you name")
-alert(`Дякую ${wathName} що ввели своє імя!`)
+// const wathName = prompt("Wath are you name")
+// alert(`Дякую ${wathName} що ввели своє імя!`)
 
-const wathDo = confirm("Ви справді хочете зайти сюди");
-console.log(wathDo)
-alert("Дякію що підтвердили")
+// const wathDo = confirm("Ви справді хочете зайти сюди");
+// console.log(wathDo)
+// alert("Дякію що підтвердили")
